cd56ba432daa01709f15272fb3e9b6d9e1d0be25
mayozesoup <dackhang.nguyen@gmail.com>
2025-09-08 10:14:06 -0400

    Changed fizz buzz

--- src/lab1/FizzBuzz.java
+++ src/lab1/FizzBuzz.java
@@ -5,9 +5,40 @@ package lab1;
  */
 class FizzBuzz {
 
+//    public static void main(String[] args) {
+//        for (int i = 1; i <= 100; i++) {
+//
+//            // Find out which numbers divide i.
+//            boolean divisibleBy3 = i % 3 == 0;
+//            boolean divisibleBy5 = i % 5 == 0;
+//
+//            // Print our appropriate result.
+//            if (divisibleBy3 && divisibleBy5) {
+//
+//                System.out.println("Fizz Buzz");
+//
+//            } else if (divisibleBy3) {
+//
+//                System.out.println("Fizz");
+//
+//            } else if (divisibleBy5) {
+//
+//                System.out.println("Buzz");
+//
+//            } else {
+//
+//                System.out.println(i);
+//
+//            }
+//        }
+//    }
     public static void main(String[] args) {
+        Fizz();
+    }
 
-        for (int i = 1; i < 100; i++) {
+    private static void Fizz() {
+        int i = 1;
+        while (i <= 100) {
 
             // Find out which numbers divide i.
             boolean divisibleBy3 = i % 3 == 0;
@@ -31,6 +62,7 @@ class FizzBuzz {
                 System.out.println(i);
 
             }
+            i++;
         }
     }
 }
