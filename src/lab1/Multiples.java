6eadb7158ca4002e4287947d18242e2266097080
mayozesoup <dackhang.nguyen@gmail.com>
2025-09-12 11:41:15 -0400

    generalized multiples

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -2,7 +2,7 @@ package lab1;
 
 public class Multiples {
     public static void main(String[] args) {
-        System.out.println(multiples());
+        System.out.println(multiples(1000, 3, 5));
     }
 
     public static int multiples(){
@@ -15,5 +15,16 @@ public class Multiples {
             i++;
         }
         return n;
+    }
+    public static int multiples(int n, int a, int b) {
+        int count = 0;
+        int i = 1;
+        while (i < n) {
+            if (i % a == 0 || i % b == 0) {
+                count++;
+            }
+            i++;
+        }
+        return count;
     }
 }
